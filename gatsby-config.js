486ba7583d901e0e6c6c7695a4fcfe3bba952579
Darren Snell <darren_snell@hotmail.co.uk>
2022-04-03 08:21:27 +0200

    personalisation

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,14 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
+    siteTitle: `Darren snell`,
+    siteTitleAlt: `Darren Snell`,
+    siteHeadline: `Darren Snell, Web Developer, Full Stack Developer`,
     siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteDescription: `A webpage to introduce myself to the world of web development.`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `Darren Snell`,
   },
   plugins: [
     {
@@ -25,9 +25,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        name: `Darren Snell`,
+        short_name: `Darren Snell`,
+        description: `An introduction to web development.`,
         start_url: `/`,
         background_color: `#141821`,
         // This will impact how browsers show your PWA/website
