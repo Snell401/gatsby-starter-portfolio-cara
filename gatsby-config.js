888d813efd844939f9e324e195037cb3556840e0
Darren Snell <darren_snell@hotmail.co.uk>
2022-04-12 11:35:30 +0200

    changes to wording

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
   plugins: [
     
     {
-      resolve: `@lekoarts/gatsby-theme-cara`,
+      resolve: ``,
       // See the theme's README for all available options
       options: {
         
